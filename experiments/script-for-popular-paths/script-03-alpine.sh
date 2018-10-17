738b48e38fdb2b8f74f6bf7fc964b67b06dc3f12
Yiwen Li <detectivelyw@gmail.com>
2018-10-17 13:19:49 -0400

    added config file, script, and gcda data for running the alpine container.

---
+++ experiments/script-for-popular-paths/script-03-alpine.sh
@@ -0,0 +1,133 @@
+#!/bin/sh
+count=26
+current=1
+
+echo "start running task [$current/$count] ..."
+uname -r
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+apk update
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+yes Y | apk add gcc
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+yes Y | apk add musl-dev
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+yes Y | apk add openssh
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+gcc hello-world.c -o hello-world
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+./hello-world
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+gcc -v
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+df -h
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+ls -la
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+which gcc
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+yes Y | apk add python
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+python hello-world.py
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+mkdir /home/test/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+cp hello-world.c /home/test/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+cp hello-world.py /home/test/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+find -name "hello-world.c"
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+grep -nr "Hello" /home/test/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+mkdir /home/output/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+tar -zcvf /home/test.tar.gz /home/test/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+tar -zxvf /home/test.tar.gz -C /home/output/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+cp -r /sys/kernel/debug/gcov/linux/ /home/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+cd /home/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+tar -zcvf gcda-data-03.tar.gz linux/
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+which scp
+echo "task [$current/$count] completed."
+current=$((current+1))
+
+echo "start running task [$current/$count] ..."
+scp gcda-data-03.tar.gz detectivelyw@10.18.171.176:~/Downloads/
+echo "task [$current/$count] completed."
+current=$((current+1))
